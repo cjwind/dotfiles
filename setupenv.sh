57893057e49e5e487f0db0b37c3ea700805978f8
zoe <cwentsai@gmail.com>
2016-11-24 10:36:11 +0800

    add git alias showing history

--- setupenv.sh
+++ setupenv.sh
@@ -47,6 +47,7 @@ $GIT config --global color.ui auto
 $GIT config --global diff.tool vimdiff
 $GIT config --global difftool.prompt false
 $GIT config --global alias.d difftool
+$GIT config --global alias.view "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cblueby %an %Cgreen(%ar)%Creset'"
 
 # link dotfiles
 test -L ~/.tcshrc || $LN -s $PWD/tcshrc ~/.tcshrc
